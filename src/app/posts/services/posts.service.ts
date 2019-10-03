2c5d44653adc1478a0861bcdaecb19bf8fc428e6
jwisniewski <jan.wisniewski@softiq.pl>
2019-10-03 14:36:35 +0200

    environments

--- src/app/posts/services/posts.service.ts
+++ src/app/posts/services/posts.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {IPostList} from '../../shared/interfaces/post-list.interface';
 import {sortByOperator} from '../../shared/helpers/sorter.helper';
+import {environment} from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,7 @@ export class PostsService {
   ) { }
 
   async getPosts() {
-    const url = 'assets/fake-posts.json';
+    const url = environment.postsUrl;
     const response = await this.http.get<IPostList>(url).pipe(
       sortByOperator('createdTime')
     ).toPromise();
