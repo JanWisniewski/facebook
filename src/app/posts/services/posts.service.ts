e3aac0c0e2057ecd5d6f511dd7eed38465d0efe1
jwisniewski <jan.wisniewski@softiq.pl>
2019-10-03 14:24:47 +0200

    sort helper

--- src/app/posts/services/posts.service.ts
+++ src/app/posts/services/posts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {IPostList} from '../../shared/interfaces/post-list.interface';
+import {sortByOperator} from '../../shared/helpers/sorter.helper';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,11 @@ export class PostsService {
     private http: HttpClient
   ) { }
 
-  getPosts() {
+  async getPosts() {
     const url = 'assets/fake-posts.json';
-    return this.http.get<IPostList>(url).toPromise();
+    const response = await this.http.get<IPostList>(url).pipe(
+      sortByOperator('createdTime')
+    ).toPromise();
+    return response;
   }
 }
